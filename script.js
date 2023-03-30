79e396a77c2fd2d22d8a945dbb5f6f0e7bffa469
Katya <taktkaterina@gmail.com>
2023-03-30 22:56:00 +0300

    first commit

--- script.js
+++ script.js
@@ -1,20 +1,55 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
 
-document.querySelector('.question').textContent = secretNumber;
+const displayGuessMessage = function (message) {
+  document.querySelector('.guess-message').textContent = message;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guessingNumber = Number(document.querySelector('.number-input').value);
   console.log(guessingNumber, typeof guessingNumber);
 
+  //No input
   if (!guessingNumber) {
-    document.querySelector('.guess-message').textContent = 'Введите число!';
+    displayGuessMessage('Введите число!');
+
+    // Player won
   } else if (guessingNumber === secretNumber) {
-    document.querySelector('.guess-message').textContent = 'Правильно!';
-  } else if (guessingNumber > secretNumber) {
-    document.querySelector('.guess-message').textContent = 'Слишком много';
-  } else if (guessingNumber < secretNumber) {
-    document.querySelector('.guess-message').textContent = 'Слишком мало';
+    displayGuessMessage('Правильно!');
+    document.querySelector('body').style.backgroundColor = '#2A7800';
+    document.querySelector('.question').style.width = '100rem';
+    document.querySelector('.question').textContent = secretNumber;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+
+    //number from input is wrong
+  } else if (guessingNumber !== secretNumber) {
+    if (score > 1) {
+      displayGuessMessage(
+        guessingNumber > secretNumber ? 'Слишком много' : 'Слишком мало'
+      );
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayGuessMessage('Игра окончена');
+      document.querySelector('.score').textContent = 0;
+    }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+
+  document.querySelector('.question').textContent = '???';
+  document.querySelector('.question').style.width = '25rem';
+  document.querySelector('.score').textContent = score;
+  displayGuessMessage('Начни угадывать');
+  document.querySelector('.number-input').value = '';
+  document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)';
 });
